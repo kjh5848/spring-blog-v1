edb84f62b326f3084d28f6f619993c07b46c3b78
juIT <153582123+kjh5848@users.noreply.github.com>
2024-01-31 09:39:31 +0900

    Revert "[fix] login noresultexceoption"

--- src/main/java/shop/mtcoding/blog/user/UserRepository.java
+++ src/main/java/shop/mtcoding/blog/user/UserRepository.java
@@ -40,14 +40,8 @@ public class UserRepository {
         Query query = em.createNativeQuery("select * from user_tb where username = ? and password = ?", User.class);
         query.setParameter(1, requestDTO.getUsername());
         query.setParameter(2, requestDTO.getPassword());
-        
-        // TODO : cjh 버그 수정
-        try {
-            User user = (User) query.getSingleResult();
-            return user;
-        }catch (Exception e){
-            return null;
-        }
 
+        User user = (User) query.getSingleResult();
+        return user;
     }
 }
