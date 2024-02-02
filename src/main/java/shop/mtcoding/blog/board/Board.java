4e4e571752bc62677ec270c90956d8b5e5982c1c
kjh5848~ <kjh5848@gmail.com>
2024-02-02 16:38:26 +0900

    230202 수업완료 조인orm

---
+++ src/main/java/shop/mtcoding/blog/board/Board.java
@@ -0,0 +1,28 @@
+package shop.mtcoding.blog.board;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import shop.mtcoding.blog.user.User;
+
+import java.time.LocalDateTime;
+
+// 오브젝트인 데이터 때문에 따로 만든다.
+@Data//get,set, tostring이 있다.
+@Entity
+@Table(name = "board_tb")
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String content;
+
+    //orm을 사용한다. 객체를 변환할 필요없이 int로 치환해서 자동을 변환해준다.
+    @ManyToOne
+    private User user;
+
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
