32cd1f8cbbeb33cbcac00eaba2e362c1b6c25cac
kjh5848~ <kjh5848@gmail.com>
2024-02-02 16:50:08 +0900

    240202 최종 완료

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,3 +1,6 @@
+
+
+-- //더미 데이터
 insert into user_tb(username, password, email, created_at) values('ssar', '1234', 'ssar@nate.com', now());
 insert into user_tb(username, password, email, created_at) values('cos', '1234', 'cos@nate.com', now());
 
