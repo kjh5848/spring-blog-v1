afdb36781d5a7365873793506bbc907027237e8e
kjh5848 <kjh5848@gmail.com>
2024-02-03 11:27:45 +0900

    [fix] 초기화면세팅완료

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -4,28 +4,13 @@ package shop.mtcoding.blog.board;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 
 @RequiredArgsConstructor
 @Controller
 public class BoardController {
 
-    private final HttpSession session;
-    private final BoardRepository boardRepository;
-
-
-    @GetMapping("/api/board/{id}")
-    public @ResponseBody Board apiboard(@PathVariable int id) {
-        return boardRepository.findById(id);
-    }
-
-
-    @GetMapping({ "/", "/board" })
+    @GetMapping({"/", "/board"})
     public String index() {
         return "index";
     }
@@ -36,10 +21,7 @@ public class BoardController {
     }
 
     @GetMapping("/board/{id}")
-    public String detail(@PathVariable int id, HttpServletRequest request) {
-        Board board = boardRepository.findById(id);
-        request.setAttribute("board", board);
-
+    public String detail() {
         return "board/detail";
     }
 }
--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,78 +1,14 @@
 package shop.mtcoding.blog.user;
 
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.HttpSession;
-
-
-/**
- * 컨트롤러
- * 1. 요청받기(URL)
- * 2. http body는 어떻게? (DTO)
- * 3. 기본 mime 전략 : x폼 (username=ssar&password=1234)
- * 4. 유효성 검사하기(body 데이터가 있다면)
- * 5. 클라이언트가 View만 원하는지? 혹은 DB처리 후 View도 원하는지?
- * 6. View만 원하면 View를 응답하면 끝
- * 7. DB처리를 원하면 Model(DAO)에게 위임 후 view를 응답하면 끝
- */
-@RequiredArgsConstructor
 @Controller
 public class UserController {
 
-    private final UserRepository userRepository;
-    private final HttpSession session;
-
-    //select이지만 로그인은 예외로 post를 한다.
-    @PostMapping("/login")
-    public String login(UserRequest.loginDTO requestDTO) {
-
-        //1. 유효성 검사
-        if (requestDTO.getUsername().length() < 3) {
-            return "error/400";
-        }
-
-        //2. 모델 연결
-        User user = userRepository.findByUsernameAndPassword(requestDTO);
-
-
-        if (user == null) {
-            return "error/401";
-        } else {
-            session.setAttribute("sessionUser", user);
-            return "redirect:/";
-        }
-
-
-    }
-
-    @PostMapping("/join")
-    public String join(UserRequest.JoinDTO requestDTO) {
-        System.out.println(requestDTO);
-
-        //1. 유효성 검사
-        if (requestDTO.getUsername().length() < 3) {
-            return "error/400";
-        }
-        //2. 동일 username 체크(나중에 하나의 트랜잭션으로 묶는 것이 좋다.)
-        User user = userRepository.findByUsername(requestDTO.getUsername());
-        if (user == null) {
-            userRepository.save(requestDTO);
-
-        } else {
-            return "error/400";
-        }
-        //3. Model에게 위임하기
-
-
-        return "redirect:/loginForm";
-    }
-
     @GetMapping("/joinForm")
     public String joinForm() {
-
         return "user/joinForm";
     }
 
@@ -88,9 +24,6 @@ public class UserController {
 
     @GetMapping("/logout")
     public String logout() {
-        // session.invalidate(); 서랍의 내용을 삭제
-        // 1번 서랍에 user가 있고 이를 삭제한다.
-        session.invalidate();
         return "redirect:/";
     }
 }
