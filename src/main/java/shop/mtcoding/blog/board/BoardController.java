f019b6595efb542e6be047ce978a5389bc7f1abc
kjh5848~ <kjh5848@gmail.com>
2024-02-06 12:00:41 +0900

    게시글 완료

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,11 +1,14 @@
 package shop.mtcoding.blog.board;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import shop.mtcoding.blog.user.User;
 
 import java.util.List;
 
@@ -14,8 +17,10 @@ import java.util.List;
 public class BoardController {
 
     private final BoardRepository boardRepository;
+    private final HttpSession session;
 
-    @GetMapping({ "/", "/board" })
+
+    @GetMapping({"/", "/board"})
     public String index(HttpServletRequest request) {
 
         List<Board> boardList = boardRepository.findAll();
@@ -24,14 +29,49 @@ public class BoardController {
         return "index";
     }
 
+    @PostMapping("/board/save")
+    public String save(BoardRequest.SaveDTO requestDTO, HttpServletRequest request) {
+        // 1. 인증체크
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        if (sessionUser == null) {
+            return "redirect:/loginForm";
+        }
+
+        // 2. 바디 데이터 확인 및 유효성 검사
+        System.out.println("requestDTO = " + requestDTO);
+
+        if (requestDTO.getTitle().length() > 10) {
+            request.setAttribute("status", 400);
+            request.setAttribute("msg", "타이틀의 길이가 10자를 초과하였습니다.");
+            return "error/40x";
+        } else if (requestDTO.getTitle().length() == 0){
+            request.setAttribute("status", 400);
+            request.setAttribute("msg", "타이틀을 입력하시오.");
+            return "error/40x";
+        }
+
+        // 3. 모델 위임
+        boardRepository.save(requestDTO,sessionUser.getId());
+
+        return "redirect:/";
+    }
+
     @GetMapping("/board/saveForm")
     public String saveForm() {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        if (sessionUser == null) {
+            return "redirect:/loginForm";
+        }
         return "board/saveForm";
     }
 
+
+
+
+
     @GetMapping("/board/{id}")
     public String detail(@PathVariable int id, HttpServletRequest request) {
-        System.out.println("id : "+id);
+        System.out.println("id : " + id);
 
         // 바디 데이터가 없으면 유효성 검사가 필요없지 ㅎ
         BoardResponse.DetailDTO responseDTO = boardRepository.findById(id);
---
+++ src/main/java/shop/mtcoding/blog/board/BoardRequest.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.blog.board;
+
+import lombok.Data;
+
+public class BoardRequest {
+
+    @Data
+    public static class SaveDTO {
+        private String title;
+        private String content;
+
+    }
+}
